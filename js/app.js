3c96da8b3e4c32d348ef88090193c629dc020359
jamiguel23 <josematthewmiguel@gmail.com>
2021-06-16 19:18:39 -0700

    added comments to js on what is needed

---
+++ js/app.js
@@ -0,0 +1,28 @@
+'use strict'
+
+
+// global variables
+  //leaderboard array
+  
+
+// foothold into DOM
+
+// construction functions
+  // user construstor has propertioes of name and score
+    //user score property
+    //user name property
+
+
+// methods and other functions
+  // game function
+    // at the start of function it will create a new user
+  //replay button
+  //local storage
+  // Event handle to get username from form
+
+
+// event listeners
+  // start game button
+  // one on each element img (3)
+  // replay button
+
